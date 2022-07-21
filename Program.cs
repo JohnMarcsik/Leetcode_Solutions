a252d4620ad3e2df9426f0655128d9e7ee74f5d7
JohnMarcsik <101882931+JohnMarcsik@users.noreply.github.com>
2022-07-21 10:11:06 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+//Console.WriteLine("test");
+namespace Leetcode
+{
+   class RomanNumeral
+   {
+      public int number;
+      public char r;
+      public RomanNumeral(char rep)
+      {
+         r = rep;
+         number = getValue();
+      }
+      private int getValue()
+      {
+         int num;
+         switch(r)
+         {
+            case 'I':
+               num = 1;
+               break;
+            case 'V':
+               num = 5;
+               break;
+            case 'X':
+               num = 10;
+               break;
+            case 'L':
+               num = 50;
+               break;
+            default:
+               num=0;
+               break;
+         }
+         return num;
+      }
+   
+
+   }
+}
